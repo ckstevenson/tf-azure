d36a39b0721e225e53513aa195d64a998325f378
Cameron Stevenson <cksteve@protonmail.com>
2021-11-05 10:37:46 +0100

    removing test info

--- roots/windows/main.tf
+++ roots/windows/main.tf
@@ -10,14 +10,14 @@ module "azurem_linux_bastion_test" {
   password       = "test1235"# Note stored in plain-text. Don't use!
   if_name        = "cameron-bastion-if"
   public_ip_name = "cameron-bastion-ip"
-  subnet_name    = "aks-subnet"
-  vnet_name      = "aks-vnet-24461701"
-  vm_rg_name     = "MC_dev_azure-dev_westus2"
-  subnet_rg_name = "MC_dev_azure-dev_westus2"
+  subnet_name    = ""
+  vnet_name      = ""
+  vm_rg_name     = ""
+  subnet_rg_name = ""
   vm_tags = {
     name       = local.vm_name
     CreatedBy  = "Cameron"
-    CreatedFor = "DevOps"
+    CreatedFor = ""
   }
   vm_sri = {
     publisher = "Canonical"
